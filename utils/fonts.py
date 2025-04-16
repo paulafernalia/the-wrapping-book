d35c4b3a61160da98341e893c1349c4329ed2d25
paulafernalia <paula.fermin.cueto@gmail.com>
2025-04-16 11:47:19 +0100

    Create carry cover page

---
+++ utils/fonts.py
@@ -0,0 +1,15 @@
+# Font configuration
+FONTCONFIG = [
+    {
+        'name': 'NotoSerifDisplay-Medium',
+        'path': "fonts/Noto_Serif_Display/static/NotoSerifDisplay-Medium.ttf"
+    },
+    {
+        'name': 'NotoSerifDisplay-Italic',
+        'path': "fonts/Noto_Serif_Display/static/NotoSerifDisplay-Italic.ttf"
+    },
+    {
+        'name': 'Poppins-Regular',
+        'path': "fonts/Poppins/Poppins-Regular.ttf"
+    }
+]
